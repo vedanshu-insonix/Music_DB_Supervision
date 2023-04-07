1912a7d935ad8851f5602812ea207ac877ec6154
riyawalia3138 <60174788+riyawalia3138@users.noreply.github.com>
2023-04-07 14:54:35 +0530

    Update urls.py

--- playlist/urls.py
+++ playlist/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path
 from . import views
-from .views import PlaylistDeleteView
+from .views import PlaylistDeleteView, TrackDeleteView
 
 urlpatterns = [
     path('', views.playlist_list, name='playlist_list'),
+    path('', views.track_list, name='track_list'),
     path('create/', views.playlist_create, name='playlist_create'),
     path('<int:pk>/', views.playlist_detail, name='playlist_detail'),
     path('<int:pk>/edit/', views.playlist_edit, name='playlist_edit'),
+    path('<int:pk>/editt/', views.track_edit, name='track_edit'),
     path('<int:pk>/delete/', PlaylistDeleteView.as_view(), name='playlist_delete'),
+    path('<int:pk>/deletee/', TrackDeleteView.as_view(), name='track_delete'),
     path('add_tracks/<int:track_id>', views.add_tracks, name='add_tracks'),
 ]
