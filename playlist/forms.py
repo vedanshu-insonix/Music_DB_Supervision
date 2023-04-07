e5b7e4da6d8275fcded4056c531aee158bb5c6d6
riyawalia3138 <60174788+riyawalia3138@users.noreply.github.com>
2023-04-07 14:55:12 +0530

    Update forms.py

--- playlist/forms.py
+++ playlist/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.forms import DateInput
-from .models import Playlist
+from .models import Playlist, PlaylistTrack
 from datetime import date
 
 class PlaylistForm(forms.ModelForm):
@@ -16,9 +16,22 @@ class PlaylistForm(forms.ModelForm):
         initial = {
             'strPlaylistDate': date.today(),
         }
+        
+        
+class TrackForm(forms.ModelForm):
+    class Meta:
+        model = PlaylistTrack
+        fields = ['sequence_order']
+                              
 
 class PlaylistDeleteForm(forms.ModelForm):
     class Meta:
         model = Playlist
         fields = ['strPlaylistName', 'strPlaylistDate', 'memPlaylistDescription']
+        
+class TrackDeleteForm(forms.ModelForm):
+    class Meta:
+        model = PlaylistTrack
+        fields = ['track']     
+        
 
